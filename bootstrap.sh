d32305bba1df5f478e21e6765d8d5ec937a9133a
Paul Moss <moss.paul@gmail.com>
2014-10-19 09:11:49 -0400

    Try to create everything as the vagrant user when possible

--- bootstrap.sh
+++ bootstrap.sh
@@ -4,18 +4,18 @@
 # and let Ansible handle the rest.
 
 # Make sure we're up to date
-sudo apt-get update
+apt-get update
 
 # Install Ansible
-sudo apt-get install -y software-properties-common
-sudo apt-add-repository -y ppa:ansible/ansible
-sudo apt-get update
-sudo apt-get install -y ansible
+apt-get install -y software-properties-common
+apt-add-repository -y ppa:ansible/ansible
+apt-get update
+apt-get install -y ansible
 
 # Run playbook to provision up VM
 # This just installs git and checks out this repo
-sudo ansible-playbook /vagrant/ansible/bootstrap.yml -i /vagrant/ansible/hosts --connection=local
+ansible-playbook /vagrant/ansible/bootstrap.yml -i /vagrant/ansible/hosts --connection=local
 
 # Run Noop for a Proof of concept
 cd vagrant-bootstrap
-sudo ansible-playbook ansible/site.yml -i ansible/hosts --connection=local
+ansible-playbook ansible/site.yml -i ansible/hosts --connection=local
